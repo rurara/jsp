39d42870d0e773b0bf1888e0b3bb373746c7c442
rurara <kgsin7@gmail.com>
2012-07-26 17:39:12 +0900

    add ignore

---
+++ src/net/hybrid/lotto/Lotto.java
@@ -0,0 +1,28 @@
+package net.hybrid.lotto;
+import java.util.Random;
+
+
+public class Lotto {
+
+	/**
+	 * @param args
+	 * 인스턴스 변수
+	 */
+	public static void main(String[] args) {
+		new Lotto().getLotto();
+	}
+	
+	public int[] getLotto() {
+		int[] numbers = new int[6];
+		for (int i = 0; i < 6; i++) {
+			numbers[i] = getNumber();
+		}
+		return numbers;
+	}
+
+	Random random = new Random();
+	private int getNumber() {
+		return random.nextInt(45) + 1;
+	}
+
+}
